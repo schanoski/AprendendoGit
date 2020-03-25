a1760fead7dcc610add7119960ea034cea086a76
William Schanoski <schanoski97@gmail.com>
2020-03-25 14:00:43 -0300

    Projeto de olá mundo com bom dia

--- ProjetoHelloWorld/Program.cs
+++ ProjetoHelloWorld/Program.cs
@@ -7,6 +7,8 @@ namespace ProjetoHelloWorld
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            Console.WriteLine("Boa tarde");
         }
     }
 }
